a233bea06ee00b6559f948f4b1110af577423a09
iospiral <iospiral@mailbox.org>
2023-05-12 03:30:22 -0400

    added comments and some styling fixed a filename

--- template/fxmanifest.lua
+++ template/fxmanifest.lua
@@ -1,29 +1,45 @@
 -- Resource Metadata
+
+-- Declare the resource manifest version for compatibility with FiveM's resource system
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+-- Set the version of the fx_server to 'cerulean'
 fx_version 'cerulean'
+
+-- Specify that this resource is for the game 'Grand Theft Auto V' ('gta5')
 game 'gta5'
 
+-- Provide author information for this resource
 author 'StellarPixelKnight <einhverrelse@proton.me>'
+
+-- Set a description for this resource
 description 'loading screen'
+
+-- Set a version number for this resource
 version '1.0.0'
 
--- What to run
+-- What to run:
+
+-- List client-side scripts to be loaded by this resource
 client_scripts {
     'client.lua'
 }
 
+-- Indicate that this resource contains map data
 this_is_a_map 'yes'
+
+-- Set the UI page for this resource
 ui_page 'index.html'
 
--- Files
+-- Files:
+
+-- List files to be included in this resource
 files {
+    'index.html',     -- HTML file for the loading screen
 
-    'index.html',
+    'styles.css',     -- CSS file for styling the loading screen
 
-    'styles.css',
+    'script.js',      -- JavaScript file containing logic for the loading screen
 
-    'script.js',
-
-    'your-video.mp4', -- replace this with the path to your video file
-
+    'your-video.mp4', -- Path to the video file to display during the loading screen, replace with your own path
 }
--- template/script.js
+++ template/script.js
@@ -1,19 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
+// Add an event listener for the 'DOMContentLoaded' event. This event is fired when the HTML document has been completely loaded.
+document.addEventListener('DOMContentLoaded', function () {
+  // Get a reference to the video element with ID 'loadingVideo'.
+  const video = document.getElementById('loadingVideo');
 
-    const video = document.getElementById('loadingVideo');
+  // Unmute the video by setting its 'muted' property to false.
+  video.muted = false;
 
-    video.muted = false;
+  // Define the name of the Lua event to be triggered when the video ends.
+  const luaEventName = 'videoEnded';
 
-
-    const luaEventName = 'videoEnded';
-
-
-    video.addEventListener('ended', function() {
-
-        // Trigger a Lua event when the video ends.
-
-        emitNet(luaEventName);
-
-    }, false);
-
+  // Add an event listener for the 'ended' event of the video, which is fired when the video playback has reached its end.
+  video.addEventListener(
+    'ended',
+    function () {
+      // Trigger a Lua event named 'videoEnded' when the video ends.
+      emitNet(luaEventName);
+    },
+    false
+  ); // The third parameter 'false' indicates that the event should be captured in the bubbling phase (not during capturing phase).
 });
